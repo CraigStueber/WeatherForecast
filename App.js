0ff9426f48aabecd15b718c46c11db7a72690240
Craig Stueber <craigstueber@gmail.com>
2023-09-11 10:23:06 -0400

    added location

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
 import { s } from "./App.style";
 import { Home } from "./pages/Home/Home";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import {ImageBackground} from "react-native";
-import backgroundImage from "./assets/background.png"
+import { ImageBackground } from "react-native";
+import backgroundImage from "./assets/background.png";
+import { useEffect, useState } from "react";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
 export default function App() {
+  const [coordinates, setCoordinates] = useState();
+  useEffect(() => {
+    getUserCorrdinates();
+  }, []);
+
+  async function getUserCorrdinates() {
+    const { status } = await requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync({});
+      setCoordinates({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    } else {
+      setCoordinates({ lat: "48.85", lng: "2.35" });
+    }
+  }
   return (
-    <ImageBackground imageStyle={s.img} style={s.img_background} source={backgroundImage}>
-    <SafeAreaProvider>
-      <SafeAreaView style={s.container}>
-        <Home />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ImageBackground
+      imageStyle={s.img}
+      style={s.img_background}
+      source={backgroundImage}
+    >
+      <SafeAreaProvider>
+        <SafeAreaView style={s.container}>
+          <Home />
+        </SafeAreaView>
+      </SafeAreaProvider>
     </ImageBackground>
   );
 }
