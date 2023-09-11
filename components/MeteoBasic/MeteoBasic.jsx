29701acce0fe169bbfd67b06ef4ff1eeef514e58
Craig Stueber <craigstueber@gmail.com>
2023-09-11 13:36:46 -0400

    Meteo Basic component made

---
+++ components/MeteoBasic/MeteoBasic.jsx
@@ -0,0 +1,23 @@
+import { View, Image } from "react-native";
+import { Txt } from "../Txt/Txt";
+import { s } from "./MeteoBasic.style";
+
+export function MeteoBasic() {
+  return (
+    <>
+      <View style={s.clock}>
+        <Txt>Clock</Txt>
+      </View>
+      <View style={s.city}>
+        <Txt>City</Txt>
+      </View>
+      <View style={s.interpretation}>
+        <Txt style={s.interpretation_txt}>Sunny</Txt>
+      </View>
+      <View style={s.temperature_box}>
+        <Txt style={s.temperature}>3&#176;</Txt>
+        <Image style={s.image} />
+      </View>
+    </>
+  );
+}
