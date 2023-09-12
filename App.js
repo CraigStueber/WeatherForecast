d53c282940f59fa8f9a2c07803d22c26f74432fe
Craig Stueber <craigstueber@gmail.com>
2023-09-12 13:26:34 -0400

    set up navigation

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { s } from "./App.style";
 import { Home } from "./pages/Home/Home";
+import { Forecasts } from "./pages/Forecasts/Forecasts";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { ImageBackground } from "react-native";
 import backgroundImage from "./assets/background.png";
@@ -10,7 +11,14 @@ import {
 } from "expo-location";
 import { MeteoAPI } from "./api/meteo";
 import { useFonts } from "expo-font";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const Stack = createNativeStackNavigator();
+const navTheme = {
+  colors: {
+    background: "transparent",
+  },
+};
 export default function App() {
   const [coordinates, setCoordinates] = useState();
   const [weather, setWeather] = useState();
@@ -52,16 +60,28 @@ export default function App() {
   }
 
   return (
-    <ImageBackground
-      imageStyle={s.img}
-      style={s.img_background}
-      source={backgroundImage}
-    >
-      <SafeAreaProvider>
-        <SafeAreaView style={s.container}>
-          {isFontLoaded && weather && <Home city={city} weather={weather} />}
-        </SafeAreaView>
-      </SafeAreaProvider>
-    </ImageBackground>
+    <NavigationContainer theme={navTheme}>
+      <ImageBackground
+        imageStyle={s.img}
+        style={s.img_background}
+        source={backgroundImage}
+      >
+        <SafeAreaProvider>
+          <SafeAreaView style={s.container}>
+            {isFontLoaded && weather && (
+              <Stack.Navigator
+                screenOptions={{ headerShown: false }}
+                initialRouteName="Home"
+              >
+                <Stack.Screen name="Home">
+                  {() => <Home city={city} weather={weather} />}
+                </Stack.Screen>
+                <Stack.Screen name="Forecasts" component={Forecasts} />
+              </Stack.Navigator>
+            )}
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </ImageBackground>
+    </NavigationContainer>
   );
 }
---
+++ pages/Forecasts/Forecasts.jsx
@@ -0,0 +1,6 @@
+import { Txt } from "../../components/Txt/Txt";
+import { s } from "./Forecasts.style";
+
+export function Forecasts({}) {
+  return <Txt>Forecasts</Txt>;
+}
